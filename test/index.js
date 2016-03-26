dfc738fef7a432294da29b5a1bda6f1114aeb631
Boom <boom@Booms-MacBook-Pro.local>
2016-03-27 00:09:54 +0700

    Add testcase

--- test/index.js
+++ test/index.js
@@ -3,7 +3,7 @@ require('mocha-generators').install()
 var Nightmare = require('nightmare')
 var expect = require('chai').expect
 
-describe('Test discounts of Harry Potter Book', function () {
+describe('E2E test Harry Potter Book', function () {
   it('amount7', function * () {
     this.timeout(15000)
     var nightmare = Nightmare()
@@ -22,7 +22,7 @@ describe('Test discounts of Harry Potter Book', function () {
     expect(value).to.equal('7')
   })
   it('discount20', function * () {
-    this.timeout(15000)
+    this.timeout(30000)
     var nightmare = Nightmare()
     var value = yield nightmare
       .goto('http://localhost:5000')
@@ -36,7 +36,7 @@ describe('Test discounts of Harry Potter Book', function () {
     expect(value).to.equal('20.00')
   })
   it('discount60', function * () {
-    this.timeout(15000)
+    this.timeout(30000)
     var nightmare = Nightmare()
     var value = yield nightmare
       .goto('http://localhost:5000')
@@ -53,7 +53,7 @@ describe('Test discounts of Harry Potter Book', function () {
     expect(value).to.equal('60.00')
   })
   it('discount60', function * () {
-    this.timeout(15000)
+    this.timeout(30000)
     var nightmare = Nightmare()
     var value = yield nightmare
       .goto('http://localhost:5000')
@@ -69,5 +69,41 @@ describe('Test discounts of Harry Potter Book', function () {
         return document.querySelector('.discount').innerHTML
       })
     expect(value).to.equal('0.00')
+  })
+  it('sumprice700(no discount)', function * () {
+    this.timeout(30000)
+    var nightmare = Nightmare()
+    var value = yield nightmare
+      .goto('http://localhost:5000')
+      .click('.btn0')
+      .click('.btn0')
+      .click('.btn0')
+      .click('.btn0')
+      .click('.btn0')
+      .click('.btn0')
+      .click('.btn0')
+
+      .evaluate(function () {
+        return document.querySelector('.calsumprice').innerHTML
+      })
+    expect(value).to.equal('700.00')
+  })
+  it('sumprice700(calc discount)', function * () {
+    this.timeout(30000)
+    var nightmare = Nightmare()
+    var value = yield nightmare
+      .goto('http://localhost:5000')
+      .click('.btn0')
+      .click('.btn1')
+      .click('.btn2')
+      .click('.btn3')
+      .click('.btn4')
+      .click('.btn5')
+      .click('.btn6')
+
+      .evaluate(function () {
+        return document.querySelector('.showsumprice').innerHTML
+      })
+    expect(value).to.equal('280.00')
   })
 })
